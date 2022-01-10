341203da5e2258032b6948b669f64b7c3b460332
Elvin Azim <elvin.azim01@gmail.com>
2022-01-10 21:51:34 +0400

    First lesson

---
+++ Lesson1.py
@@ -0,0 +1,60 @@
+# <editor-fold desc="Escape Sequences and print">
+
+# print("'Hello World'.\n\t\t\t©Elvin Azimov")
+# print('"Hello World".\n\t\t\t©Elvin Azimov')
+#
+# print("\"Hello World\".\n\t\t\t©Elvin Azimov")
+# print('\'Hello World\'.\n\t\t\t©Elvin Azimov')
+
+
+# print("Hello", end=" ")
+# print("World")
+# print("Elvin")
+
+# print(1, 2, 3, 4, 5, sep="!")   # separate - ayirmaq
+# print("Hello", "World", sep="@", end=" ")
+# print("!")
+
+# </editor-fold>
+
+# <editor-fold desc="Data Types">
+
+# int - integer - tam ədəd
+# bool - boolean - məntiqi məlumat növü
+# float - floating number - kəsr ədəd
+# str - string - Mətn
+# chr - char - character(Bir simvol)
+
+# </editor-fold>
+
+# <editor-fold desc="Variables">
+
+# Naming Cases:
+# 1. Camel case - middleName
+# 2. Pascal case - MiddleName
+# 3. Snake case(python) - middle_name
+# 4. Kebab case - (middle-name)
+
+# name = "Elvin"
+# a = int(5)
+# b = 5
+
+
+# </editor-fold>
+
+
+# <editor-fold desc="Input">
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Entre second number: "))
+
+result = number1 + number2
+
+print(number1, "+", number2, "=", result)
+
+print(f"{number1} + {number2} = {result}")  # string formatting
+
+print("{} + {} = {}" .format(number1, number2, result))
+
+# </editor-fold>
+
